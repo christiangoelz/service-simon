d3cd5c5d8919bd26fba74e9bc4524bfcfd740e43
Federated Secure Computing <federatedsecurecomputing@lrz.uni-muenchen.de>
2023-01-08 17:24:42 +0100

    add test framework

---
+++ test/interface.py
@@ -0,0 +1,52 @@
+from federatedsecure.server.server.bus import Bus
+
+
+class TestInterface:
+
+    def __init__(self):
+        self.bus = Bus()
+
+    def post(self, *path, body=None):
+        if len(path) == 1:
+            if path[0] == 'representations':
+                response = self.bus.create_representation(body)
+                return {'type': 'uuid', 'uuid': response}, 200
+        raise RuntimeError()
+
+    def put(self, *path, body=None):
+        if len(path) == 1:
+            if path[0] == 'representations':
+                response = self.bus.upload_representation(body)
+                return {'type': 'uuid', 'uuid': response}, 200
+        raise RuntimeError()
+
+    def patch(self, *path, body=None):
+        if len(path) == 2:
+            if path[0] == 'representation':
+                response = self.bus.call_representation(path[1], body)
+                if response is None:
+                    return {'type': 'none'}, 200
+                return {'type': 'uuid', 'uuid': response}, 200
+        raise RuntimeError()
+
+    def get(self, *path):
+        if len(path) == 1:
+            if path[0] == 'representations':
+                response = self.bus.list_representations()
+                return {'type': 'list', 'list': response}, 200
+        if len(path) == 2:
+            if path[0] == 'representation':
+                response = self.bus.download_representation(path[1])
+                return {'type': 'object', 'object': response}, 200
+        if len(path) == 3:
+            if path[0] == 'representation':
+                uuid = self.bus.create_attribute(path[1], path[2], public=True)
+                return {'type': 'uuid', 'uuid': uuid}, 200
+        raise RuntimeError()
+
+    def delete(self, *path):
+        if len(path) == 2:
+            if path[0] == 'representation':
+                self.bus.release_representation(path[1])
+                return {'type': 'none'}, 200
+        raise RuntimeError()

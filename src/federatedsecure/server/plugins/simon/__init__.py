e28bbcd43f08429784551a019fc99ee4934e4b4f
Federated Secure Computing <federatedsecurecomputing@lrz.uni-muenchen.de>
2023-01-08 17:17:32 +0100

    import precursor codebase (renamed and sanitized)

---
+++ src/federatedsecure/server/plugins/simon/__init__.py
@@ -0,0 +1,13 @@
+from federatedsecure.server.plugins.simon.microservice import MicroserviceSimon
+
+
+def federatedsecure_register(registry):
+
+    registry.register(
+        {
+            "namespace": "federatedsecure",
+            "protocol": "Simon",
+            "version": "0.6.0"
+        },
+        MicroserviceSimon()
+    )
